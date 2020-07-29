a45ea1ed40bf82e470d4c2c68e9c499b3f412378
Akil Harris <akil.harris@firstlook.org>
2020-07-29 10:26:53 -0400

    style sort bar

--- frontend/components/Pages/SortBar.vue
+++ frontend/components/Pages/SortBar.vue
@@ -1,12 +1,20 @@
 <style scoped>
 .sortbar {
-  margin: 5px 0 0 0;
+  margin: 10px 0 5px 0;
+  background-color: aliceblue;
+  padding: 10px 5px 8px 10px;
+  width: 35vw;
+}
+
+.sort-header {
+  display: inline-block;
+  margin: 0 5px 0 0;
 }
 </style>
 
 <template>
   <div class="sortbar">
-    <div>Sort By:</div>
+    <span class="sort-header">Sort By:</span>
     <select v-model="selectedSort" v-on:change="sortChangeHandler">
       <option
         v-for="option in options"
