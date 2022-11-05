4f258533b5f3d598c3792293fce938911a81a1a5
Micah Lee <micah@micahflee.com>
2022-11-05 11:58:29 -0700

    Use createWebHistory instead of createWebHashHistory

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import App from './App.vue';
 import Structure from './Pages/Structure.vue';
@@ -19,7 +19,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes
 });
 
